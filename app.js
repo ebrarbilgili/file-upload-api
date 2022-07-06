54d12872776474d907cb69362c66b88147eaa6ef
Ebrar <ebrarbilgili@outlook.com>
2022-07-06 11:37:47 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+
+global.__basedir = __dirname;
+
+var corsOptions = {
+    origin: "http://localhost:3001"
+};
+
+app.use(cors(corsOptions));
+
+const initRoutes = require("./routes/routes");
+
+app.use(express.urlencoded({ extended: true }));
+
+initRoutes(app);
+
+let port = 3000;
+
+app.listen(port, () => {
+    console.log(`Running at localhost:${port}`);
+});
